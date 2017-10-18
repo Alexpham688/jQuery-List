7370a422c37297cbda5b538e7aabf0fac05c6ec2
Alexpham688 <Alexpham688@gmail.com>
2017-10-17 21:10:10 -0400

    another commit

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 $(function(){
-    //adding to do item to source 
+    //adding to do item to need(source)
   $('#add-btn').click(function(e){
-    e.preventDefault();
     var toAdd = $('#input').val().toUpperCase();
-    $('ol.source').append('<li class="list-group-item">' + toAdd + '</li>');
-    if(toAdd == ""){
+    $('ol.source').prepend('<li class="list-group-item">' + toAdd + '</li>');
+    $(this).addClass('active');
+    if(toAdd === ""){
       alert('Add something!!');
     }
     $('#input').val('');
+    e.preventDefault();
   });
 
   //adding active class to list item with click
-  $('.list-group-item').click(function(){
+  $('.list-group').on('click','.list-group-item',function(){
     $(this).parent().find('.list-group-item').removeClass('active');
     $(this).addClass('active');
 
@@ -24,6 +25,7 @@ $(function(){
     $(this).attr('disabled', true);
     $('.add').attr('disabled', true);
   };
+  
   //make remove button disabled until item selected
   var back = $(this).parents('.pick-list').find('.destination').children('.active');
   if(back.length > 0) {
@@ -34,18 +36,19 @@ $(function(){
     $('.remove').attr('disabled', true);
   };
  });
+
+ //move item from need(source) to done(destination) when item is selected
   $('.add').click(function(){
     var item = $(this).parents('.pick-list').find('.source').children('.active');
-    //move item from source to destination when item is selected
     if(item.length > 0) {
       $('.source .list-group-item.active').appendTo('.destination');
       $('.add').attr('disabled', true);
       $('.destination .list-group-item.active').removeClass('active');
     }
   });
+  //moving item back to need(source) from done(destination)
   $('.remove').click(function(){
     var item = $(this).parents('.pick-list').find('.destination').children('.active');
-    //move item back to original spot
     if(item.length > 0){
       $('.destination .list-group-item.active').appendTo('.source');
       $('.remove').attr('disabled',true);
@@ -53,8 +56,40 @@ $(function(){
     }
   });
 
+    //moving all items from need to done
+    $('#add-all').click(function(e){
+      var select = $('.source li');
+      if(select.length === 0){
+        alert('Nothing to move...');
+        e.preventDefault();
+      }
+      $('.destination').append($(select).clone());
+      $(select).remove();
+      // e.preventDefault();
+    });
+    //moving all items from  done(desintation) back to need(source)
+    $('#remove-all').click(function(e){
+      var deselect = $('.destination li');
+      if(deselect.length === 0){
+        alert('Nothing to move...');
+        e.preventDefault();
+      }
+      $('.source').append($(deselect).clone());
+      $(deselect).remove();
+    })
 
+    //delete all items from done(destination)
+    $('#delete').on('click',function() {
+      var select = $('.destination li');
+      var trash = $('.destination li').remove();
+    if(select.length === 0){
+      alert('Nothing to delete');
+    } else {
+      return trash;
+    }
 
+    });
+
+  //makes list items sortable
   $('ol').sortable();
-
 });
