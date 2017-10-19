0efb2aa999ec0859e0624864674116ab486c39e2
Alexpham688 <Alexpham688@gmail.com>
2017-10-19 09:14:09 -0400

    another commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(function(){
     //adding to do item to need(source)
-  $('#add-btn').click(function(e){
+  $('#add-btn').on('click',function(e){
     var toAdd = $('#input').val().toUpperCase();
     $('ol.source').prepend('<li class="list-group-item">' + toAdd + '</li>');
     $(this).addClass('active');
@@ -25,7 +25,6 @@ $(function(){
     $(this).attr('disabled', true);
     $('.add').attr('disabled', true);
   };
-  
   //make remove button disabled until item selected
   var back = $(this).parents('.pick-list').find('.destination').children('.active');
   if(back.length > 0) {
@@ -38,7 +37,7 @@ $(function(){
  });
 
  //move item from need(source) to done(destination) when item is selected
-  $('.add').click(function(){
+  $('.add').on('click',function(){
     var item = $(this).parents('.pick-list').find('.source').children('.active');
     if(item.length > 0) {
       $('.source .list-group-item.active').appendTo('.destination');
@@ -47,7 +46,7 @@ $(function(){
     }
   });
   //moving item back to need(source) from done(destination)
-  $('.remove').click(function(){
+  $('.remove').on('click',function(){
     var item = $(this).parents('.pick-list').find('.destination').children('.active');
     if(item.length > 0){
       $('.destination .list-group-item.active').appendTo('.source');
@@ -55,9 +54,8 @@ $(function(){
       $('.source .list-group-item.active').removeClass('active');
     }
   });
-
     //moving all items from need to done
-    $('#add-all').click(function(e){
+    $('#add-all').on('click',function(e){
       var select = $('.source li');
       if(select.length === 0){
         alert('Nothing to move...');
@@ -68,7 +66,7 @@ $(function(){
       // e.preventDefault();
     });
     //moving all items from  done(desintation) back to need(source)
-    $('#remove-all').click(function(e){
+    $('#remove-all').on('click',function(e){
       var deselect = $('.destination li');
       if(deselect.length === 0){
         alert('Nothing to move...');
@@ -81,15 +79,64 @@ $(function(){
     //delete all items from done(destination)
     $('#delete').on('click',function() {
       var select = $('.destination li');
-      var trash = $('.destination li').remove();
-    if(select.length === 0){
-      alert('Nothing to delete');
-    } else {
-      return trash;
-    }
+      var trash = $('.destination li').fadeOut(1500);
+    });
 
+    //removing each list item seperately with double click
+    $(document).on('dblclick','.destination li', function(){
+      $(this).addClass('strike').css({'background-color':'#E40000','color':'#000'}).fadeOut(1500);
     });
 
   //makes list items sortable
   $('ol').sortable();
+  //adding autocomplete
+  var tags = [
+      "ActionScript",
+      "AppleScript",
+      "Ask",
+      "Basic",
+      "Buy",
+      "Climb",
+      "Clean",
+      "Close",
+      "Casket",
+      "ColdFusion",
+      'Doing',
+      'Dry',
+      'Eat',
+      "Erlang",
+      "Fortran",
+      "Get",
+      "Go",
+      'Forward',
+      "Handle",
+      "Java",
+      "JavaScript",
+      "Listen",
+      "Get dryclean",
+      "Put",
+      "Grocery shop",
+      "Walk the dog",
+      "Scrub",
+      'Wash',
+     'Clear the table',
+     'Lower the roof',
+     'Start the dishwasher',
+     'wipe the table',
+     'clean the stove',
+     'clean the sink',
+     'sweep the floor',
+     'wash the floor',
+     'empty the dishwasher',
+     'put dishes away',
+     'water the plants',
+     'wash the car',
+     'mow the lawn',
+     'paint the fence',
+     'tree',
+     'try'
+    ];
+    $('#input').autocomplete({
+      source:tags
+    });
 });
